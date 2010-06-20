591cd8bcc7ba3c98883c5f5d2d3270c5d526ac0f
Nick Fitzgerald <fitzgen@gmail.com>
2010-06-19 21:35:00 -0700

    Busting out a bunch more user and repo access

--- github.js
+++ github.js
@@ -2,10 +2,10 @@
     var
 
     // Keep authentication variables private.
-    auth_username,
-    auth_token,
+    authUsername,
+    authToken,
 
-    api_root = "https://github.com/api/v2/json/",
+    apiRoot = "https://github.com/api/v2/json/",
 
     jsonp = function (url, callback, context) {
         var id = +new Date,
@@ -17,12 +17,52 @@
         };
 
         url += "?callback=" + encodeURIComponent("gh.__jsonp_callbacks["+ id +"]");
-        if (auth_username && auth_token) {
-            url += "&login=" + auth_username + "&auth_token=" + auth_token;
+        if (authUsername && authToken) {
+            url += "&login=" + authUsername + "&authToken=" + authToken;
         }
-        script.src = api_root + url;
+        script.setAttribute("src", apiRoot + url);
 
         document.getElementsByTagName('head')[0].appendChild(script);
+    },
+
+    post = function (url, vals) {
+        var
+        form = document.createElement("form"),
+        iframe = document.createElement("iframe"),
+        doc = iframe.contentDocument !== undefined ?
+            iframe.contentDocument :
+            iframe.contentWindow.document;
+        key, field;
+        vals = vals || {};
+
+        form.setAttribute("method", "post");
+        form.setAttribute("action", apiRoot + url);
+        for (key in vals) {
+            if (vals.hasOwnProperty(key)) {
+                field = document.createElement("input");
+                field.type = "hidden";
+                field.value = encodeURIComponent(vals[key]);
+                form.appendChild(field);
+            }
+        }
+
+        iframe.setAttribute("style", "display: none;");
+        doc.body.appendChild(form);
+        document.body.appendChild(iframe);
+        form.submit();
+    },
+
+    authRequired = function (username) {
+        if (!authUsername || !authToken || authUsername !== username) {
+            throw new TypeError("gh: Must be authenticated to do that.");
+        }
+    },
+
+    paramify = function (params) {
+        var str = "", key;
+        for (key in params) if (params.hasOwnProperty(key))
+            str += key + "=" + params[key] + "&";
+        return str.replace(/&$/, "");
     },
 
     // Expose global gh variable and keep a local variable.
@@ -31,24 +71,129 @@
     gh.__jsonp_callbacks = {};
 
     gh.authenticate = function (username, token) {
-        auth_username = username;
-        auth_token = token;
+        authUsername = username;
+        authToken = token;
         return this;
     };
 
-    gh.user = {};
+    /*
+     * Users
+     */
+
+    gh.user = function (username) {
+        if ( !(this instanceof gh.user)) {
+            return new gh.user(username);
+        }
+        this.username = username;
+    };
+    gh.user.prototype.show = function (callback, context) {
+        jsonp("user/show/" + this.username, callback, context);
+        return this;
+    };
+    gh.user.prototype.update = function (params) {
+        authRequired(this.username);
+        var key, postData = {
+            login: authUsername,
+            token: authToken
+        };
+        for (key in params) {
+            if (params.hasOwnProperty(key)) {
+                postData["values["+key+"]"] = encodeURIComponent(params[key]);
+            }
+        }
+        post("user/show/" + this.username, postData);
+        return this;
+    };
+    gh.user.prototype.following = function (callback, context) {
+        jsonp("user/show/" + this.username + "/following", callback, context);
+    };
+    gh.user.prototype.followers = function (callback, context) {
+        jsonp("user/show/" + this.username + "/followers", callback, context);
+    };
+    gh.user.prototype.follow = function (user) {
+        authRequired.call(this);
+        post("user/follow/" + user);
+        return this;
+    };
+    gh.user.prototype.unfollow = function (user) {
+        authRequired.call(this);
+        post("user/unfollow/" + user);
+        return this;
+    };
+    gh.user.prototype.watching = function (callback, context) {
+        jsonp("repos/watched/" + this.username, callback, context);
+        return this;
+    };
+    gh.user.prototype.repos = function (callback, context) {
+        gh.repo.forUser(this.username, callback, context);
+        return this;
+    };
+    gh.user.prototype.forkRepo = function (user, repo) {
+        authRequired(this.username);
+        post("repos/fork/" + user + "/" + repo);
+        return this;
+    };
+
     gh.user.search = function (query, callback, context) {
         jsonp("user/search/" + query, callback, context);
         return this;
     };
-    gh.user.show = function (username, callback, context) {
-        // The username argument is optional (defaults to the authenticated
-        // user), so if it is missing, shift the arguments down.
-        if (typeof username === "function") {
-            context = arguments[1];
-            callback = arguments[0];
-            username = "";
+
+    /*
+     * Repositories
+     */
+
+    gh.repo = function (user, repo) {
+        if ( !(this instanceof gh.repo)) {
+            return new gh.repo(user, repo);
         }
-        jsonp("user/show/" + username, callback, context);
+        this.repo = repo;
+        this.user = user;
     };
-}(window))
+    gh.repo.prototype.show = function (callback, context) {
+        jsonp("repos/show/" + this.user + "/" + this.repo, callback, context);
+        return this;
+    };
+    gh.repo.prototype.update = function (params) {
+        authRequired(this.user);
+        var key, postData = {
+            login: authUsername,
+            token: authToken
+        };
+        for (key in params) {
+            if (params.hasOwnProperty(key)) {
+                postData["values["+key+"]"] = encodeURIComponent(params[key]);
+            }
+        }
+        post("repos/show/" + this.user + "/" + this.repo, postData);
+        return this;
+    };
+    // TODO: visibility, collaborators, contributors, network, languages, tags, branches
+
+    gh.repo.search = function (query, opts, callback, context) {
+        var url = "repos/search/" + query.replace(" ", "+");
+        if (typeof opts === "function") {
+            opts = {};
+            callback = arguments[1];
+            context = arguments[2];
+        }
+        url += "?" + paramify(opts);
+        return this;
+    };
+    gh.repo.forUser = function (user, callback, context) {
+        jsonp("repos/show/" + user, callback, context);
+        return this;
+    };
+    gh.repo.create = function (name, opts) {
+        authRequired(authUsername);
+        opts.name = name;
+        post("repos/create", opts);
+        return this;
+    };
+    gh.repo.del = function (name) {
+        authRequired(authUsername);
+        post("repos/delete/" + name);
+        return this;
+    };
+
+}(window));
