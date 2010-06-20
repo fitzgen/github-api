b319110e10aef1911c2d5307fe880fa48899a682
Nick Fitzgerald <fitzgen@gmail.com>
2010-06-20 02:41:42 -0700

    Adding code comments

--- github.js
+++ github.js
@@ -5,8 +5,11 @@
     authUsername,
     authToken,
 
+    // Always using JSON because it is the only way to leverage JSONP.
     apiRoot = "https://github.com/api/v2/json/",
 
+    // Send a JSONP request to the Github API that calls `callback` with
+    // `context` as `this`.
     jsonp = function (url, callback, context) {
         var id = +new Date,
         script = document.createElement("script");
@@ -25,6 +28,8 @@
         document.getElementsByTagName('head')[0].appendChild(script);
     },
 
+    // Send an HTTP POST. Unfortunately, it isn't possible to support a callback
+    // with the resulting data. (Please prove me wrong!)
     post = function (url, vals) {
         var
         form = document.createElement("form"),
@@ -52,12 +57,15 @@
         form.submit();
     },
 
+    // Throw a TypeError if the user is not authenticated properly.
     authRequired = function (username) {
         if (!authUsername || !authToken || authUsername !== username) {
             throw new TypeError("gh: Must be authenticated to do that.");
         }
     },
 
+    // Convert an object to a url parameter string, e.g. {foo:1, bar:3} ->
+    // "foo=1&bar=3".
     paramify = function (params) {
         var str = "", key;
         for (key in params) if (params.hasOwnProperty(key))
@@ -68,8 +76,11 @@
     // Expose global gh variable and keep a local variable.
     gh = globals.gh = {};
 
+    // Psuedo private home for JSONP callbacks (which are required to be global
+    // by the nature of JSONP).
     gh.__jsonp_callbacks = {};
 
+    // Authenticate a user. Does not try to validate at any point.
     gh.authenticate = function (username, token) {
         authUsername = username;
         authToken = token;
@@ -86,10 +97,14 @@
         }
         this.username = username;
     };
+
+    // Show basic user info. More info if authenticated.
     gh.user.prototype.show = function (callback, context) {
         jsonp("user/show/" + this.username, callback, context);
         return this;
     };
+
+    // Update user info. Must be authenticated as this user.
     gh.user.prototype.update = function (params) {
         authRequired(this.username);
         var key, postData = {
@@ -104,36 +119,53 @@
         post("user/show/" + this.username, postData);
         return this;
     };
+
+    // Find out who this user is following.
     gh.user.prototype.following = function (callback, context) {
         jsonp("user/show/" + this.username + "/following", callback, context);
     };
+
+    // Find out who is following this user.
     gh.user.prototype.followers = function (callback, context) {
         jsonp("user/show/" + this.username + "/followers", callback, context);
     };
+
+    // Make this user follow some other user. Must be authenticated as this
+    // user.
     gh.user.prototype.follow = function (user) {
         authRequired.call(this);
         post("user/follow/" + user);
         return this;
     };
+
+    // Quit following the given user. Must be authenticated as this user.
     gh.user.prototype.unfollow = function (user) {
         authRequired.call(this);
         post("user/unfollow/" + user);
         return this;
     };
+
+    // Find out who this user is watching.
     gh.user.prototype.watching = function (callback, context) {
         jsonp("repos/watched/" + this.username, callback, context);
         return this;
     };
+
+    // Find out what repos this user has.
     gh.user.prototype.repos = function (callback, context) {
         gh.repo.forUser(this.username, callback, context);
         return this;
     };
+
+    // Fork the repo that lives at http://github.com/user/repo. Must be
+    // authenticated.
     gh.user.prototype.forkRepo = function (user, repo) {
         authRequired(this.username);
         post("repos/fork/" + user + "/" + repo);
         return this;
     };
 
+    // Search users for `query`.
     gh.user.search = function (query, callback, context) {
         jsonp("user/search/" + query, callback, context);
         return this;
@@ -150,10 +182,19 @@
         this.repo = repo;
         this.user = user;
     };
+
+    // Get information on this repo.
     gh.repo.prototype.show = function (callback, context) {
         jsonp("repos/show/" + this.user + "/" + this.repo, callback, context);
         return this;
     };
+
+    // Update information on this repo. Must be authenticated. Params can include:
+    //   * description
+    //   * homepage
+    //   * has_wiki
+    //   * has_issues
+    //   * has_downloads
     gh.repo.prototype.update = function (params) {
         authRequired(this.user);
         var key, postData = {
@@ -170,6 +211,8 @@
     };
     // TODO: visibility, collaborators, contributors, network, languages, tags, branches
 
+    // Search for repositories. `opts` may include `start_page` or `language`,
+    // which must be capitalized.
     gh.repo.search = function (query, opts, callback, context) {
         var url = "repos/search/" + query.replace(" ", "+");
         if (typeof opts === "function") {
@@ -180,16 +223,22 @@
         url += "?" + paramify(opts);
         return this;
     };
+
+    // Get all the repos that are owned by `user`.
     gh.repo.forUser = function (user, callback, context) {
         jsonp("repos/show/" + user, callback, context);
         return this;
     };
+
+    // Create a repository. Must be authenticated.
     gh.repo.create = function (name, opts) {
         authRequired(authUsername);
         opts.name = name;
         post("repos/create", opts);
         return this;
     };
+
+    // Delete a repository. Must be authenticated.
     gh.repo.del = function (name) {
         authRequired(authUsername);
         post("repos/delete/" + name);
