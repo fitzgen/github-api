ac19564a0fd57464f9428bba55786b4e4b86c7c4
avyaznikov <varkasha@mail.ru>
2012-01-25 15:47:44 +0400

    new gh-issue fixed

--- github.js
+++ github.js
@@ -538,7 +538,7 @@
 
     gh.issue = function (user, repo, number) {
         if ( !(this instanceof gh.issue) )
-            return new gh.commit(user, repo, number);
+            return new gh.issue(user, repo, number);
         this.user = user;
         this.repo = repo;
         this.number = number;
